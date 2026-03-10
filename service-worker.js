c0f6e29224f86e210d0a5624c706730c807e0d30
AJAY <ajjayyadav8290@gmail.com>
2026-03-10 21:48:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
 
+self.addEventListener("install", e => {
+
+e.waitUntil(
+caches.open("memoryvault").then(cache => {
+
+return cache.addAll([
+"/",
+"/index.html",
+"/style.css",
+"/script.js"
+])
+
+})
+)
+
+})
